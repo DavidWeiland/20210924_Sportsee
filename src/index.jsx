43773641bc402bb8894211ffb7bf02c6e6ca8291
David Weiland <weiland.david@icloud.com>
2021-09-30 15:03:06 +0200

    architecture

---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './utils/style/index.css';
+import Home from './pages/Home';
+import HorizontalNavBar from './components/HorizontalNavBar'
+import VerticalNavBar from './components/VerticalNavBar'
+import Error from './components/Error'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <HorizontalNavBar />
+      <div className="row">
+        <VerticalNavBar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route>
+            <Error />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/utils/style/Atoms.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+import colors from "./colors"
+import styled, { keyframes } from "styled-components"
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+export const Loader = styled.div`
+    padding: 10px;
+    border: 6px solid ${colors.primary};
+    border-bottom-color: transparent;
+    border-radius: 22px;
+    animation: ${rotate} 1s infinite linear;
+    height: 0;
+    width: 0;
+`
+
+export const StyledLink = styled(Link)`
+    padding: 10px 15px;
+    color: ${colors.fontColorLight};
+    text-decoration: none;
+    font-size: 24px;
+    font-weight:500;
+    text-align: center;
+`
