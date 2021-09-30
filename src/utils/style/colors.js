bd750b97358e5e31d294fa0527eec4984e466e51
David Weiland <weiland.david@icloud.com>
2021-09-30 15:04:11 +0200

    css

---
+++ src/utils/style/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  primary:'#5843E4',
+  secondary: '#8186A0',
+  background: '#FFFFFF',
+  fontColorDark: '#000000',
+  fontColorLight: '#FFFFFF',
+  fontColorRed:'#FF0000',
+  navBackground: '#000000',
+}
+
+export default colors
