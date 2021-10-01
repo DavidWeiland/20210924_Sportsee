3af05aa91ce9019f95fecc2ab84656e1ae2d7060
David Weiland <weiland.david@icloud.com>
2021-10-01 11:32:32 +0200

    fix bug fetchData

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -11,7 +11,7 @@ export function useFetch(url) {
         async function fetchData() {
             try {    
                 const response = await fetch(url)
-                const data = await response.json()
+                const {data} = await response.json()
                 setData(data)
             } catch (error) {
                 console.log('===== error =====', error)
