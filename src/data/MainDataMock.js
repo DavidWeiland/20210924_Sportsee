001ba0f43b665e0c7480e4b6adc76d31e2049be2
David Weiland <weiland.david@icloud.com>
2021-10-01 08:42:05 +0200

    assets/icon and data changes

---
+++ src/data/MainDataMock.js
@@ -0,0 +1,32 @@
+export const MAINDATA = [
+    {
+        id: 12,
+        userInfos: {
+            firstName: 'Karl',
+            lastName: 'Dovineau',
+            age: 31,
+        },
+        todayScore: 0.12,
+        keyData: {
+            calorieCount: 1930,
+            proteinCount: 155,
+            carbohydrateCount: 290,
+            lipidCount: 50
+        }
+    },
+    {
+        id: 18,
+        userInfos: {
+            firstName: 'Cecilia',
+            lastName: 'Ratorez',
+            age: 34,
+        },
+        todayScore: 0.3,
+        keyData: {
+            calorieCount: 2500,
+            proteinCount: 90,
+            carbohydrateCount: 150,
+            lipidCount: 120
+        }
+    }
+]
