ed8f575902a2f12bd7ae1c5907f07d9b9fe71ca9
David Weiland <weiland.david@icloud.com>
2021-10-07 12:12:10 +0200

    working radialComponent

--- src/components/RadialBarChartComponent/index.jsx
+++ src/components/RadialBarChartComponent/index.jsx
@@ -1,28 +1,37 @@
-import { ResponsiveContainer } from 'recharts';
-//import { useFetch } from '../../utils/hooks';
-import { Loader } from '../../utils/style/Atoms';
+import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
+import colors from '../../utils/style/colors';
 
-function RadialBarChartComponent({userId}) {
-  //const { data, error, isLoading } = useFetch(`http://localhost:5000/user/${userId}/performance`)
-  //console.log(data)
+function RadialBarChartComponent({ data }) {
+  const datas = [
+    {
+      "todayScore": 1,
+      "fill":"none"
+    },
+    {
+      "todayScore": data.todayScore,
+    }
+  ]
   
-  const isLoading = true
-
-  //if (error) {
-  //  return (
-  //    <Loader/>
-  //  )
-  //}
-
-  if (isLoading) {
+  const CustomizedLegend = () => {
     return (
-        <Loader />
+      <div>
+        <div>
+            <span>Score</span>
+        </div>
+        <div style={{ textAlign:"center", width:"30%", position:'absolute', top:65, left:55 }}>
+          <span style={{fontSize:26, fontWeight:700 }}>{data.todayScore * 100}%</span><br/>
+          <span style={{fontSize:16, fontWeight:500, opacity:0.5 }}>de votre objectif</span>
+        </div>
+      </div>
     )
   }
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-        
+      <RadialBarChart width="100%" height="100%" innerRadius="45%" outerRadius="70%" data={datas} startAngle={225} endAngle={-134}>
+        <RadialBar background={false} dataKey="todayScore" fill={`${colors.fontColorRed}`}/>
+        <Legend align='left' verticalAlign='top' iconSize={0} wrapperStyle={{ marginTop: 24, marginLeft: 30, width: '100%', color: `${colors.fontColorDark}`, fontSize: 15, backgroundColor:"transparent" }} content={CustomizedLegend} />
+      </RadialBarChart>
     </ResponsiveContainer>
   );
 }
