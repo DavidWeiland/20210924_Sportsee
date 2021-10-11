aff3c7ed2d838917d3c8e64abed2063cc5076d04
David Weiland <weiland.david@icloud.com>
2021-10-11 12:07:21 +0200

    move Error to pages/

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+const ErrorContainer = styled.div`
+  width:100%;
+  height:933px;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content: center;
+`
+
+const Title = styled.h1`
+  margin:0;
+  font-size:300px;
+`
+const SubTitle = styled.h2`
+  margin:0;
+  font-size:40px;
+`
+
+function Error() {
+  return (
+    <ErrorContainer>
+      <Title>
+        {`404`}
+      </Title>
+      <SubTitle>
+        {`Oups, la page demandée n'existe pas`}
+      </SubTitle>
+    </ErrorContainer>
+  )
+}
+
+export default Error
