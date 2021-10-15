9a215551baa0846e6934a5e76da9fedf72a956bf
David Weiland <weiland.david@icloud.com>
2021-10-15 15:26:36 +0200

    modify path

--- src/utils/Variables/index.js
+++ src/utils/Variables/index.js
@@ -1,1 +1,6 @@
-export const path = `http://localhost:3000/user/`
+/** As soon as the final api is online, change the value of firstPath
+ * @type { String}
+ */
+const firstPath = `http://localhost:3000/`
+
+export const path = `${firstPath}user/`
